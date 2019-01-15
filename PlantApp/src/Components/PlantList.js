87fec2ba5269ed672de7ea09ede109fd695e84d8
Melissa O'Hearn <melissa.ohearn@gmail.com>
2019-01-15 11:57:43 -0800

    take out react-native-elements

--- PlantApp/src/Components/PlantList.js
+++ PlantApp/src/Components/PlantList.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { FlatList } from 'react-native';
 import { connect } from 'react-redux';
 // import ListItem from './ListItem';
-import { List, ListItem } from 'react-native-elements';
+//import { List, ListItem } from 'react-native-elements';
 import { plantsFetch } from '../actions';
 import PlantListItem from './PlantListItem'
 
@@ -11,21 +11,12 @@ import PlantListItem from './PlantListItem'
 class PlantList extends Component {
   componentDidMount() {
     this.props.plantsFetch();
-    console.log("IN COMPONENT DID MOUNT");
-    const plants = this.props.plantsFetch()
-    console.log("plants",plants);
-    console.log("props",this.props);
-
   }
 
   renderItem({ item }) {
     return (
-      <ListItem
-        roundAvatar
-        item={item}
-        avatar={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhfI6nhteYYX3KD2MgxefTqbBitSeafZgEwRG4ImtKL4BAEgrv' }}
-        title={item.nickname}
-        subtitle={item.genusSpecies}
+      <PlantListItem
+        plant={item}
         />
     );
   }
@@ -33,15 +24,11 @@ class PlantList extends Component {
 render() {
   console.log(this.props);
     return (
-      <List containerStyle={{ flex: 1 }}>
         <FlatList
           data={this.props.plants}
           renderItem={this.renderItem}
           keyExtractor={(item, index) => index.toString}
           />
-      </List>
-
-
       );
   }
 }
